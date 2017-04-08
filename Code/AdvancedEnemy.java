e08bbfbb34eb7f78baeb6a0a82a181509d44d48c
JonatanNilsson <nilsson.jonatan98@gmail.com>
2017-04-08 14:27:01 +0200

    Update AdvancedEnemy.java

--- Code/AdvancedEnemy.java
+++ Code/AdvancedEnemy.java
@@ -3,21 +3,50 @@ import java.awt.Image;
 
 public class AdvancedEnemy extends BasicEnemy{
 	
+	int hiddenRandom;
 	
 	AdvancedEnemy(int x, int y, ID id, int size, int health, Image img) {
 		super(x, y, id, size, health, img);
+		this.hiddenRandom = (int)(Math.random() * 299);
 	}
 	
 	public void tick(Handler handler){
 		super.tick(handler);
 		
-		if (Game.tickCounter % 300 == 0 && freeSight(handler)){
-			handler.addObject(new Bomb(this.getX(), this.getY(), 1, 1, ID.ENEMY_BOMB));
+		if (Game.tickCounter % 300 == this.hiddenRandom && freeSight(handler)){
+			
+			int dirX, dirY;
+			
+			dirX = (handler.player.getX() / Game.TILE_SIZE - this.getX() / Game.TILE_SIZE);
+			if (dirX > 1){
+				dirX = 1;
+			}
+			else if (dirX < -1){
+				dirX = -1;
+			}
+			else{
+				dirX = 0;
+			}
+			
+			dirY = (handler.player.getY() / Game.TILE_SIZE - this.getY() / Game.TILE_SIZE);
+			if (dirY > 1){
+				dirY = 1;
+			}
+			else if (dirY < -1){
+				dirY = -1;
+			}
+			else{
+				dirY = 0;
+			}
+			
+			handler.addObject(new Bomb(this.getX(), this.getY(), dirX, dirY, ID.ENEMY_BOMB));
+		
 		}
 		
 	}
 	
 	
+	// Check if there is a wall in between the enemy and the player
 	private boolean freeSight(Handler handler){
 		double x = this.getX(), y = this.getY();
 		for (int i = 0; i < 50; i++){
@@ -33,3 +62,4 @@ public class AdvancedEnemy extends BasicEnemy{
 	
 	
 }
+
